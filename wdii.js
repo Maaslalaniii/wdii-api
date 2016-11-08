bb79df0cf9bc5de8dc8ecda7548478d7c93f17d6
Maas Lalani <maas@live.ca>
2016-11-07 22:25:48 -0500

    Inserted comments and semicolons

--- wdii.js
+++ wdii.js
@@ -1,27 +1,34 @@
-const cheerio = require('cheerio-req')
+// Cheerio + Tiny-req
+const cheerio = require('cheerio-req');
 
+// Function getDay(cb) to get and return data.
 module.exports.getDay = (send) => {
-
-  let days = []
-
+  
+  let days = [];
+  
+  // Attempt to connect and scrape html.
   cheerio('http://opan.cf/day', (err, $) => {
+    
+    // Handle error should there be one.
     if (err) {
-      send('Could not retrieve data.')
+      send('Could not retrieve data.');
     }
     
+    // Loop through to find three days. [yesterday, today, tomorrow]
     for (let i = 0; i < 3; i++) {
       
+      // Create a day object with the date, day, and events properties.
       let day = {
         date: $('.day h3').eq(i).html(),
         day: $('.day h1').eq(i).html(),
         events: $('.day p').eq(i).html().split('<br>')
-      }
+      };
       
-      days[i] = day
-
+      // Insert day object into the corresponding position.
+      days[i] = day;
     }
 
-    send(JSON.stringify(days))
-
-  })
+    // Send JSON data to the server through the callback.
+    send(JSON.stringify(days));
+  });
 }
