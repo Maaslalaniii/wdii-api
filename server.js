1a9b0514bc12df206020a928dbadad542139365a
Maas Lalani <maas@live.ca>
2016-11-07 22:29:58 -0500

    Added semicolons
    Appended semicolons to the end of the necessary lines for consistency. Added few comments as needed.

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const wdii = require('./wdii')
-const http = require('http')
-const port = process.env.PORT || 3000
+// Import the create module, to call the function.
+const wdii = require('./wdii');
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-const requestHandler = (request, response) => {  
+const requestHandler = (request, response) => {
+  
+  // Call the function with a callback which displays the returned data.
   wdii.getDay((data) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end(data)
-  })
+    response.end(data);
+  });
 }
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
-server.listen(port, '0.0.0.0')
+server.listen(port, '0.0.0.0');
 
